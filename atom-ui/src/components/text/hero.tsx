296bb4577f112b69625481e292969c73011b0d52
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-03-13 13:29:20 +0100

    dist watcher

--- atom-ui/src/components/text/hero.tsx
+++ atom-ui/src/components/text/hero.tsx
@@ -2,12 +2,12 @@ import {FC} from "react";
 
 interface TextProps {
 	style?: "primary" | "secondary" | "accent" | "white" | "black";
-	content: string;
+	children: string;
 }
 
 
-export const HeroText: FC<TextProps> = ({style = "primary", content}) => {
-	return <h1 className={`text-7xl font-bold uppercase text-${style}`}>{content}</h1>;
+export const HeroText: FC<TextProps> = ({style = "primary", children}) => {
+	return <h1 className={`text-7xl font-bold uppercase text-${style}`}>{children}</h1>;
 }
 
 
