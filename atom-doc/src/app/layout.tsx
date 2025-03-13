31f87b578276876586d3fe5034cfd92be617142a
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-03-13 14:41:12 +0100

    updates to export - works for module

--- atom-doc/src/app/layout.tsx
+++ atom-doc/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "atom-ui/dist/index.css";
 import React from "react";
 
 const geistSans = Geist({
