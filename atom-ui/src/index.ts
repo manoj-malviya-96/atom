48585bec101035b147be0c08388e4367b53765f5
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-03-14 18:05:57 +0100

    faced

--- atom-ui/src/index.ts
+++ atom-ui/src/index.ts
@@ -1,6 +1,6 @@
 import '@/global.css';
 
-export * from '@/components/common/element';
+export * from '@/components/common';
 export * from '@/components/text';
 export * from '@/components/layout';
 export * from '@/components/button';
