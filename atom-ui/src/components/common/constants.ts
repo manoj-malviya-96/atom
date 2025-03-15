1f3f090be5ac08ecd2f03131083906de32bc319d
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-03-15 16:45:33 +0100

    update style

--- atom-ui/src/components/common/constants.ts
+++ atom-ui/src/components/common/constants.ts
@@ -7,5 +7,5 @@ export const TransitionDuration_ms = 100
 
 // TW = TailwindCSS classes
 export const TW_TransitionDuration = 'duration-100'
-export const TW_PrimaryBorder = 'bg-primary/3 border-primary/7 border'
+export const TW_PrimaryBorder = 'bg-transparent border-primary/7 border'
 export const TW_PrimaryBorderWithHover = 'bg-primary/3 border-primary/30 border hover:border-primary transition'
